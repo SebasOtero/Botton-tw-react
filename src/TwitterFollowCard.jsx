a27a0801a81fcff79b87878390e76bbcd8a9ffcc
Sebastian Otero <87873376+SebasOtero@users.noreply.github.com>
2024-08-05 19:14:52 -0300

    Descripción de los cambios

--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,19 +1,23 @@
-import {useState} from 'react' 
+import { useState } from 'react' // Importa el hook useState de React
 
-export function TwitterFollowCard({  children,  userName = "unknown"}) {
-
+export function TwitterFollowCard({ children, userName = "unknown" }) {
+  // Define el estado inicial isFollowing como false
   const [isFollowing, setIsFollowing] = useState(false);
 
-  const text = isFollowing ? "Siguiendo" : "Seguir"
+  // Define el texto del botón basado en el estado isFollowing
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+  
+  // Define la clase del botón basada en el estado isFollowing
   const buttonClassName = isFollowing
     ? "tw-followCard-button is-following"
     : "tw-followCard-button";
 
+  // Maneja el clic en el botón para cambiar el estado isFollowing
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
 
-    const handleClick = () => {
-      setIsFollowing(!isFollowing);
-    }
-
+  // Retorna el JSX del componente
   return (
     <article className="tw-followCard">
       <header className="tw-followCard-header">
@@ -22,13 +26,11 @@ export function TwitterFollowCard({  children,  userName = "unknown"}) {
           alt="El avatar de midudev"
           src={`https://unavatar.io/${userName}`}
         />
-
         <div className="tw-follorCard-info">
-          <strong>{children} </strong>
+          <strong>{children}</strong>
           <span className="tw-followCard-infoUserName"> @{userName}</span>
         </div>
       </header>
-
       <aside>
         <button className={buttonClassName} onClick={handleClick}>{text}</button>
       </aside>
